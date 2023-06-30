9b333e55e7ab25204b9e83888f9819c861403259
John-Flavian <flavianomeokwe@gmail.com>
2023-06-30 11:56:20 +0100

    adds the route that handles post requests on the projects.

--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -1,6 +1,22 @@
 import { NextResponse } from "next/server";
 import projects from './data.json';
+import {v4 as uuidv4} from 'uuid';
 
 export async function GET(request) {
+    return NextResponse.json(projects);
+}
+
+export async function POST(request) {
+    const { title, desc, url } = await request.json();
+    // console.log({ title, desc, url });
+    const newProject = {
+        id: uuidv4(),
+        title,
+        desc,
+        url
+    };
+
+    projects.push(newProject);
+
     return NextResponse.json(projects);
 }
